012c7b436484d82daadf6ee04f91933e6bde4389
akbarrahmatm <akbarrahmatmulyatama@gmail.com>
2023-01-01 11:35:24 +0700

    Upload init file

---
+++ app/src/main/java/com/akbarrahmatm/osm_maps_direction/MainActivity.java
@@ -0,0 +1,14 @@
+package com.akbarrahmatm.osm_maps_direction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
